6ee54f497aaf275e2164cd48aa0b37931e553c1c
Akash0406 <startx0406@gmail.com>
2021-12-06 03:17:56 +0530

    Second Commit: Almost complete logic and style are completed

---
+++ assets/js/home.js
@@ -0,0 +1,44 @@
+// alert("it's Connected");
+
+
+
+const test = document.getElementsByClassName("category-item");
+console.log(test);
+
+for (const element of test) {
+    const category = element.innerHTML;
+    console.log(category);
+    if (category == "Cleaning") {
+        element.style.backgroundColor = "#064635";
+    } else if (category == "Work") {
+        element.style.backgroundColor = "#4C0070";
+    } else if (category == "Personal") {
+        element.style.backgroundColor = "#0F4C75";
+    } else if (category == "School") {
+        element.style.backgroundColor = "#52616B";
+    } else if (category == "Other") {
+        element.style.backgroundColor = "#8E806A";
+    }
+};
+
+
+
+
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    let todayDate = today.getDate();
+    let month = today.getMonth();
+    let Year = today.getFullYear;
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML = todayDate + "/" + month + "  " + h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+    return i;
+}
--- index.js
+++ index.js
@@ -1,24 +1,79 @@
 const express = require('express');
 const port = 3000;
-const app = express();
-const path = require('path')
+const path = require('path');
 
 const db = require('./config/mongoose')
 const List = require('./models/list')
 
+const app = express();
 app.set('view engine', 'ejs');
-app.set('view', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded());
 app.use(express.static('assets'));
 
 
+var month = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUNE', 'JULY', 'AGST', 'SEPT', 'OCT', 'NOV', 'DEC']
 app.get('/', function (req, res) {
     List.find({}, function (err, list) {
         if (err) {
             console.log(`Error while fetching list From DB`);
+            return;
         }
+        return res.render('home', {
+            title: 'To-Do List',
+            to_do_list: list,
+            Month: month
+        });
     })
 })
+
+
+app.post('/create-task', function (req, res) {
+    console.log(req.body.description);
+    console.log(req.body.category);
+    console.log(req.body.date);
+    console.log(req.body);
+    // contactList.push(req.body);
+    List.create({
+        description: req.body.description,
+        category: req.body.category,
+        date: req.body.date
+    }, function (err, newTask) {
+        if (err) {
+            console.log('error in creating a task');
+            return;
+        }
+        console.log('***********', newTask);
+
+        return res.redirect('back');
+    })
+})
+
+
+
+app.get('/delete-task/', function (req, res) {
+    // get the id from Query in the url
+    let id = req.query.id;
+    console.log(id);
+
+    // find the contact in the database using id and delete
+    List.findByIdAndDelete(id, function (err) {
+        if (err) {
+            console.log('error in deleting an object from database');
+            return;
+        }
+        return res.redirect('back');
+    })
+    // let phone = req.query.phone;
+    // removeByAttr(contactList, 'phone', phone);
+    // let contactIndex = contactList.findIndex(a => a.phone === phone)
+    // if (contactIndex != -1) {
+    // contactList.splice(contactIndex, 1);
+    // }
+
+
+})
+
 
 app.listen(port, function (err) {
     if (err) {
